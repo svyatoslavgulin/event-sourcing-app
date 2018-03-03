a9d8168f60e8b611a47fd5e10dda89b796257e1c
Sergey Ilminskih <sergey.ilminskih@ardec.com>
2018-03-03 17:16:13 +0400

    Added separating of object into fields.

--- src/main/java/ru/ardecs/handlers/CommonHandler.java
+++ src/main/java/ru/ardecs/handlers/CommonHandler.java
@@ -1,11 +1,17 @@
 package ru.ardecs.handlers;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.commons.lang3.tuple.MutablePair;
+import org.apache.commons.lang3.tuple.Pair;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
-import ru.ardecs.service.TemporaryStoreService;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.io.IOException;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.Objects;
+import java.util.stream.Stream;
 
 /**
  * @author <a href="mailto:sergey.ilminskih@ardecs.com">Sergey Ilminskih</a>
@@ -13,12 +19,57 @@ import java.util.List;
 @Component
 public class CommonHandler<T> {
 
-    @Autowired
-    private TemporaryStoreService storeService;
+    private static final Logger LOG = LoggerFactory.getLogger(CommonHandler.class);
 
-    public boolean update(T entity) {
-        List<String> filds = new ArrayList<>();
-        
-        return true;
+    private final ObjectMapper mapper = new ObjectMapper();
+
+    public <T> T update(String body, Class<? extends T> clazz, String id) {
+
+        LOG.info("Start process updating of " + clazz.getTypeName());
+        T entity = getEntity(body, clazz);
+
+        if (Objects.isNull(entity)) {
+            LOG.info(clazz.getTypeName() + " is empty.");
+            return entity;
+        }
+
+        Stream.of(entity.getClass().getDeclaredFields())
+                .map(field -> retrieveNameAndValueField(field, entity))
+                .filter(Objects::nonNull)
+                .forEach(pair -> {
+                    LOG.info("Here, I must get result of execution Slava's classes");
+                });
+
+        return entity;
+    }
+
+    private <T> Pair<String, String> retrieveNameAndValueField(Field field, T entity) {
+
+        for (Method method : entity.getClass().getMethods()) {
+            if (isGetMethodCurrentField(field, method)) {
+                try {
+                    return new MutablePair<>(field.getName(), (String) method.invoke(entity));
+                } catch (Exception e) {
+                    LOG.error(e.getMessage(), e);
+                }
+            }
+        }
+
+        LOG.info("Could not get the value of the field " + field.getName());
+        return null;
+    }
+
+    private boolean isGetMethodCurrentField(Field field, Method method) {
+        return method.getName().toLowerCase().contains(field.getName().toLowerCase())
+                && method.getName().contains("get");
+    }
+
+    private <T> T getEntity(String body, Class<? extends T> clazz) throws IllegalArgumentException {
+
+        try {
+            return mapper.readValue(body, clazz);
+        } catch (IOException e) {
+            throw new IllegalArgumentException(e);
+        }
     }
 }
