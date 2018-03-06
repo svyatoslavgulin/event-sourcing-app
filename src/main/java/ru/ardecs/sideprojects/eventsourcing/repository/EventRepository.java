9f944712bdfebf026cb9287608d4e482cb7021dc
Svyatoslav Gulin <svyatoslav.gulin@gmail.com>
2018-03-06 09:46:17 +0400

    [ref] remove unused import

--- src/main/java/ru/ardecs/sideprojects/eventsourcing/repository/EventRepository.java
+++ src/main/java/ru/ardecs/sideprojects/eventsourcing/repository/EventRepository.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import ru.ardecs.sideprojects.eventsourcing.model.Event;
-import ru.ardecs.sideprojects.eventsourcing.model.EventType;
 
 public interface EventRepository extends MongoRepository<Event, String> {
     List<Event> findAllByObjectIdAndName(String objectId, String name);
