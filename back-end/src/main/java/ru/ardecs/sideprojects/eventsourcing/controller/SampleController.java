29793eb1984efa915b7062d3ba869bf6b2cd3e80
Svyatoslav Gulin <svyatoslav.gulin@gmail.com>
2018-05-28 09:23:08 +0400

    Revert "[upd] разделение контроллера для запросов и контроллера для команд"
    This reverts commit 4e8c093d8deb6b385e95e4b4377209dcb417e4eb.

--- back-end/src/main/java/ru/ardecs/sideprojects/eventsourcing/controller/SampleController.java
+++ back-end/src/main/java/ru/ardecs/sideprojects/eventsourcing/controller/SampleController.java
@@ -4,35 +4,47 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ru.ardecs.sideprojects.eventsourcing.handlers.CommonHandler;
 import ru.ardecs.sideprojects.eventsourcing.model.HeroEntity;
 import ru.ardecs.sideprojects.eventsourcing.service.TemporaryStoreService;
 
 import java.lang.reflect.InvocationTargetException;
+import java.util.List;
 
 /**
  * @author <a href="mailto:srgeyilminskih@ardecs.com">Sergey Ilminskih</a>
  */
 @RestController
-public class SampleCommandController {
+public class SampleController {
 
-    private static final Logger LOG = LoggerFactory.getLogger(SampleCommandController.class);
+    private static final Logger LOG = LoggerFactory.getLogger(SampleController.class);
 
     @Autowired
     private CommonHandler<HeroEntity> commonHandler;
     @Autowired
     private TemporaryStoreService storeService;
 
+    @GetMapping("/")
+    public String version() {
+        return "it is test version: 1";
+    }
+
+    @GetMapping("/heroes")
+    @CrossOrigin(origins = "http://localhost:4200")
+    public List<HeroEntity> getHeroes() {
+        return storeService.getEntitys();
+    }
+
+    @GetMapping("/heroes/{id}")
+    @CrossOrigin(origins = "http://localhost:4200")
+    public HeroEntity getHeroes(@PathVariable String id) {
+        return storeService.getEntityById(id);
+    }
+
     @PostMapping("/heroes")
     @CrossOrigin(origins = "http://localhost:4200")
-    public HeroEntity saveHero(@RequestBody HeroEntity body) throws JsonProcessingException, InvocationTargetException, IllegalAccessException {
+    public HeroEntity save(@RequestBody HeroEntity body) throws JsonProcessingException, InvocationTargetException, IllegalAccessException {
         HeroEntity entity = commonHandler.create(body, HeroEntity.class);
         storeService.save(entity);
         return entity;
@@ -40,7 +52,7 @@ public class SampleCommandController {
 
     @PutMapping("/heroes")
     @CrossOrigin(origins = "http://localhost:4200")
-    public HeroEntity updateHero(@RequestBody HeroEntity body) throws JsonProcessingException, InvocationTargetException, IllegalAccessException {
+    public HeroEntity updateHeroes(@RequestBody HeroEntity body) throws JsonProcessingException, InvocationTargetException, IllegalAccessException {
         LOG.info("Body id: " + body.getId());
         LOG.info("Body name: " + body.getName());
         HeroEntity entity = commonHandler.update(body, HeroEntity.class);
@@ -49,7 +61,7 @@ public class SampleCommandController {
 
     @DeleteMapping("/heroes/{id}")
     @CrossOrigin(origins = "http://localhost:4200")
-    public void deleteHero(@PathVariable String id) throws JsonProcessingException {
+    public void deleteHeroe(@PathVariable String id) throws JsonProcessingException {
         storeService.delete(id);
         commonHandler.delete(HeroEntity.class, id);
     }
