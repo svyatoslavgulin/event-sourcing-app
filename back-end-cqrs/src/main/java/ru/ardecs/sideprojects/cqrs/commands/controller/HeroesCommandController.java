d8a4d4775c6ef20bc149f954f094e87e3a390402
Svyatoslav Gulin <svyatoslav.gulin@gmail.com>
2018-05-28 10:10:19 +0400

    [upd] remove illegal implementation

--- back-end-cqrs/src/main/java/ru/ardecs/sideprojects/cqrs/commands/controller/HeroesCommandController.java
+++ back-end-cqrs/src/main/java/ru/ardecs/sideprojects/cqrs/commands/controller/HeroesCommandController.java
@@ -1,9 +1,7 @@
 package ru.ardecs.sideprojects.cqrs.commands.controller;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,10 +10,8 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import ru.ardecs.sideprojects.eventsourcing.handlers.CommonHandler;
-import ru.ardecs.sideprojects.eventsourcing.model.HeroEntity;
 
-import java.lang.reflect.InvocationTargetException;
+import ru.ardecs.sideprojects.eventsourcing.model.HeroEntity;
 
 /**
  * @author <a href="mailto:srgeyilminskih@ardecs.com">Sergey Ilminskih</a>
@@ -25,30 +21,23 @@ import java.lang.reflect.InvocationTargetException;
 public class HeroesCommandController {
     private static final Logger LOG = LoggerFactory.getLogger(HeroesCommandController.class);
 
-    private final CommonHandler<HeroEntity> commonHandler;
-
-    @Autowired
-    public HeroesCommandController(CommonHandler<HeroEntity> commonHandler) {
-        this.commonHandler = commonHandler;
-    }
-
     @PostMapping("/heroes")
     @CrossOrigin(origins = "http://localhost:4200")
-    public void saveHero(@RequestBody HeroEntity body) throws JsonProcessingException, InvocationTargetException, IllegalAccessException {
-        commonHandler.create(body, HeroEntity.class);
+    public void saveHero(@RequestBody HeroEntity body) {
+        //TODO: do something
     }
 
     @PutMapping("/heroes")
     @CrossOrigin(origins = "http://localhost:4200")
-    public void updateHero(@RequestBody HeroEntity body) throws JsonProcessingException, InvocationTargetException, IllegalAccessException {
+    public void updateHero(@RequestBody HeroEntity body) {
         LOG.info("Body id: " + body.getId());
         LOG.info("Body name: " + body.getName());
-        commonHandler.update(body, HeroEntity.class);
+        //TODO: do something
     }
 
     @DeleteMapping("/heroes/{id}")
     @CrossOrigin(origins = "http://localhost:4200")
-    public void deleteHero(@PathVariable String id) throws JsonProcessingException {
-        commonHandler.delete(HeroEntity.class, id);
+    public void deleteHero(@PathVariable String id) {
+        //TODO: do something
     }
 }
