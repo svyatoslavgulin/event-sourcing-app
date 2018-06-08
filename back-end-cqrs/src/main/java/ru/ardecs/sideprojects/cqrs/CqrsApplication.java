271a19a8125afb1aeafb69a0900bddee87de09e1
Svyatoslav Gulin <svyatoslav.gulin@gmail.com>
2018-06-08 08:36:56 +0400

    [ref]

--- back-end-cqrs/src/main/java/ru/ardecs/sideprojects/cqrs/CqrsApplication.java
+++ back-end-cqrs/src/main/java/ru/ardecs/sideprojects/cqrs/CqrsApplication.java
@@ -1,6 +1,5 @@
 package ru.ardecs.sideprojects.cqrs;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
